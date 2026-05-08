78bb32f6355049343600a4a017aa95d13caf5731
MatheusQCardoso <matheusqcardoso98@gmail.com>
2026-05-07 21:55:33 -0300

    [1.1.0] Hash Correction 1.1.0

--- Formula/qpm.rb
+++ Formula/qpm.rb
@@ -5,10 +5,10 @@ class Qpm < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/MatheusQCardoso/homebrew-qpm/releases/download/1.1.0/qpm-1.1.0-darwin-arm64.tar.gz"
-    sha256 "99acd06baf3aa219606d45ca17628f156c01bc4ff95ece3dd52b528315414e22"
+    sha256 "a2eccfb9768647497bad33a08fe19a9d3ccf8e5b29a3f216507546f115bc81a5"
   else
     url "https://github.com/MatheusQCardoso/homebrew-qpm/releases/download/1.1.0/qpm-1.1.0-darwin-amd64.tar.gz"
-    sha256 "febabf6f71837f514870d8b707d8892a9ea0edffd04d522f541cdac8f7e9b07b"
+    sha256 "a55350037466d2938c022200934b9c0ce43bc083052acfc541b7e1d31a73729f"
   end
 
   def install
