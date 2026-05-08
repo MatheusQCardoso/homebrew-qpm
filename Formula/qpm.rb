3553eb771a5a012ca04ba36fd7b21efef0090639
MatheusQCardoso <matheusqcardoso98@gmail.com>
2026-05-07 21:33:13 -0300

    [1.1.0] Formula Update

--- Formula/qpm.rb
+++ Formula/qpm.rb
@@ -1,14 +1,14 @@
 class Qpm < Formula
   desc "Quirino's Package Manager"
   homepage "https://github.com/MatheusQCardoso/homebrew-qpm/"
-  version "1.0.0"
+  version "1.1.0"
 
   if Hardware::CPU.arm?
-    url "https://github.com/MatheusQCardoso/homebrew-qpm/releases/download/1.0.0/qpm-1.0.0-darwin-arm64.tar.gz"
-    sha256 "d3e5dd3f375804c355d458665696e865ba2248667d5be847c63f8bfafe3b5fe2"
+    url "https://github.com/MatheusQCardoso/homebrew-qpm/releases/download/1.1.0/qpm-1.1.0-darwin-arm64.tar.gz"
+    sha256 "f277ec0ff297dca900653fe933ef548c4dbad541245d426e06ef8f5f734817a5"
   else
-    url "https://github.com/MatheusQCardoso/homebrew-qpm/releases/download/1.0.0/qpm-1.0.0-darwin-amd64.tar.gz"
-    sha256 "eaac5dd0208a54f125e6c3cd8bc0b4c96ee50f3983e3809a77cf79c5b2b3117e"
+    url "https://github.com/MatheusQCardoso/homebrew-qpm/releases/download/1.1.0/qpm-1.1.0-darwin-amd64.tar.gz"
+    sha256 "851d10a4e0f1fde108c8711f42d2814837f14a3e332330e49be78ce5c14a7521"
   end
 
   def install
