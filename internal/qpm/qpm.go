e4a683552ce8cfca27f43fad943086dbd6825994
MatheusQCardoso <matheusqcardoso98@gmail.com>
2026-05-09 18:46:43 -0300

    [1.2.0] Adds installation summary

--- internal/qpm/qpm.go
+++ internal/qpm/qpm.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/MatheusQCardoso/homebrew-qpm/internal/fs"
 	"github.com/MatheusQCardoso/homebrew-qpm/internal/graph"
@@ -109,11 +111,81 @@ func Install(ctx context.Context, opt InstallOptions) error {
 	}
 	log.Verbosef("wrote lockfile: %s", lockPath)
 
+	start := time.Now()
 	log.Infof("installing...")
 	installer := NewInstaller(packagesDir, log)
 	if err := installer.InstallAll(ctx, g); err != nil {
 		return err
 	}
+	duration := time.Since(start).Round(time.Second)
 	log.Infof("done")
+	printInstallSummary(log, g, duration, packagesDir)
 	return nil
+}
+
+func printInstallSummary(log Logger, g *graph.Graph, duration time.Duration, packagesDir string) {
+	total, qpmCount, spmCount := installSummaryCounts(g)
+	size, err := directorySize(packagesDir)
+	if err != nil {
+		log.Verbosef("unable to compute packages directory size: %v", err)
+	}
+	log.Infof("\nInstalled %d packages", total)
+	log.Infof("  • QPM: %d", qpmCount)
+	log.Infof("  • SPM: %d", spmCount)
+	if err == nil {
+		log.Infof("  • Total size: %s", formatBytes(size))
+	}
+	log.Infof("  • Time: %s", formatDuration(duration))
+}
+
+func directorySize(root string) (int64, error) {
+	var total int64
+	err := filepath.WalkDir(root, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		info, err := os.Lstat(path)
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		total += info.Size()
+		return nil
+	})
+	return total, err
+}
+
+func installSummaryCounts(g *graph.Graph) (total, qpmCount, spmCount int) {
+	if g == nil {
+		return 0, 0, 0
+	}
+	total = len(g.Nodes)
+	for _, spec := range g.Nodes {
+		switch spec.Type {
+		case model.DependencyTypeQPM:
+			qpmCount++
+		case model.DependencyTypeSPM:
+			spmCount++
+		}
+	}
+	return total, qpmCount, spmCount
+}
+
+func formatDuration(d time.Duration) string {
+	return d.String()
+}
+
+func formatBytes(bytes int64) string {
+	switch {
+	case bytes >= 1<<30:
+		return fmt.Sprintf("%.1fGB", float64(bytes)/float64(1<<30))
+	case bytes >= 1<<20:
+		return fmt.Sprintf("%.1fMB", float64(bytes)/float64(1<<20))
+	case bytes >= 1<<10:
+		return fmt.Sprintf("%.1fKB", float64(bytes)/float64(1<<10))
+	default:
+		return fmt.Sprintf("%dB", bytes)
+	}
 }
